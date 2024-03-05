56d5dd294e44d2a95b5b9e785656b1a152c0de53
Dolly Aggarwal <dollyaggarwal1712@gmail.com>
2024-03-05 15:04:41 +0530

    workflow added

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,7 @@ export { default as Hero } from "./img/hero.png";
 export { default as Leaf1 } from "./img/Leaf.png";
 export { default as Leaf2 } from "./img/leaf2.png";
 export { default as about } from "./img/about.png";
-export {default as Resume} from "./CV.pdf"
+export {default as Resume} from "./Resume.pdf"
 
 // projects
 
